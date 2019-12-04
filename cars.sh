a4de1124f4029775210e37886f0c3c2a41d8d7fb
Joseph Sneifer <sneifer@chapman.edu>
2019-12-03 23:25:30 -0800

    Adds cars.sh, My_old_cars.txt, and README.md

--- cars.sh
+++ cars.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
 #cars.sh
 #Joseph Sneifer
-Continue=true
 while :
 do 
 	echo "1. Enter a new car"
 	echo "2. Display a list of cars"
 	echo "3. Quit and exit the program"
-	read answer
+	read -r answer
 	case $answer in
-		"1") read -p "Enter year: " year
-			read -p "Enter make: " make
-			read -p "Enter model: " model
+		"1") read -rp "Enter year: " year
+			read -rp "Enter make: " make
+			read -rp "Enter model: " model
 			NewCar="$year:$make:$model"
 			echo "$NewCar"
 			echo "$NewCar" >> My_old_cars.txt;;	
-		"2") while read p;
+		"2") while read -r p;
 		do
 			echo "$p"
 		done <My_old_cars.txt;;
